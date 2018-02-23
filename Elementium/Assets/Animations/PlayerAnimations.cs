dd6c00add5d9213421495c41c1587f3ffb669a37
Jake Sharkey <jakesharkey12@gmail.com>
2018-02-23 00:39:13 -0500

    Animation folder

---
+++ Elementium/Assets/Animations/PlayerAnimations.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+
+    Animator animator;
+
+    public float speedliftWall = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.speed = speedliftWall;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        bool moveforward = Input.GetKey("w");
+        animator.SetBool("walk", moveforward);
+
+        bool liftwall = Input.GetKeyDown("r");
+        animator.SetBool("liftWall", liftwall);
+
+    }
+}
